894067b1f99d24ad206cd1fe34bee87a955d6415
CintiaKarolliny <79120035+CintiaKarolliny@users.noreply.github.com>
2023-04-26 22:11:53 -0300

    Initial commit

---
+++ src/app/tabuada/tabuada.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabuada',
+  templateUrl: './tabuada.component.html',
+  styleUrls: ['./tabuada.component.css'],
+})
+export class TabuadaComponent implements OnInit {
+  n: number = 7;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  getNumbers() {
+    let arr = [];
+    for (let i = 1; i <= this.n; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+
+  tabuar() {
+    let arr = [];
+    for (let i = 1; i <= 10; i++) {
+      arr.push(this.n * i);
+    }
+    return arr;
+  }
+}
